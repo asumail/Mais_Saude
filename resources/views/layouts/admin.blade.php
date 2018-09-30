5e0da92a3762e733c5a51bbf2b2b9b4e8a77a002
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-30 11:05:14 +0200

    Adicionando o Template Dashboard

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <!-- Title -->
+        <title>Medical</title>
+        
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="author" content="Steelcoders" />
+        
+        <!-- Styles -->
+        <link href="{{ URL::asset('css/css.css')}}" rel='stylesheet' type='text/css'>
+        <link href="{{ URL::asset('assets/plugins/pace-master/themes/blue/pace-theme-flash.css')}}" rel="stylesheet"/>
+        <link href="{{ URL::asset('assets/plugins/uniform/css/uniform.default.min.css')}}" rel="stylesheet"/>
+        <link href="{{ URL::asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/plugins/fontawesome/css/font-awesome.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/plugins/line-icons/simple-line-icons.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/offcanvasmenueffects/css/menu_cornerbox.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/waves/waves.min.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/switchery/switchery.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/plugins/3d-bold-navigation/css/style.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/plugins/slidepushmenus/css/component.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/weather-icons-master/css/weather-icons.min.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/metrojs/MetroJs.min.css')}}" rel="stylesheet" type="text/css"/>	
+        <link href="{{ URL::asset('assets/plugins/toastr/toastr.min.css')}}" rel="stylesheet" type="text/css"/>	
+        	
+        <!-- Theme Styles -->
+        <link href="{{ URL::asset('assets/css/modern.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/css/themes/green.css')}}" class="theme-color" rel="stylesheet" type="text/css"/>
+        <link href="{{ URL::asset('assets/css/custom.css')}}" rel="stylesheet" type="text/css"/>
+        
+        <script src="{{ URL::asset('assets/plugins/3d-bold-navigation/js/modernizr.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/offcanvasmenueffects/js/snap.svg-min.js')}}"></script>
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        @yield('cssLocal')
+        
+    </head>
+    <body class="page-header-fixed">
+        @include('layouts.header')
+        <main class="page-content content-wrap">
+            @include('layouts.sidebar')
+            <div class="page-inner">
+                <div id="main-wrapper">
+                    @yield('content')
+                </div><!-- Main Wrapper -->
+                @include('layouts.footer')
+            </div><!-- Page Inner -->
+        </main><!-- Page Content -->
+        
+        <div class="cd-overlay"></div>
+	
+
+        <!-- Javascripts -->
+        <script src="{{ URL::asset('assets/plugins/jquery/jquery-2.1.4.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/pace-master/pace.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/jquery-blockui/jquery.blockui.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/switchery/switchery.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/uniform/jquery.uniform.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/offcanvasmenueffects/js/classie.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/offcanvasmenueffects/js/main.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/waves/waves.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/3d-bold-navigation/js/main.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/waypoints/jquery.waypoints.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/jquery-counterup/jquery.counterup.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/toastr/toastr.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/flot/jquery.flot.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/flot/jquery.flot.time.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/flot/jquery.flot.symbol.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/flot/jquery.flot.resize.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/curvedlines/curvedLines.js')}}"></script>
+        <script src="{{ URL::asset('assets/plugins/metrojs/MetroJs.min.js')}}"></script>
+        <script src="{{ URL::asset('assets/js/modern.js')}}"></script>
+        <script src="{{ URL::asset('assets/js/pages/dashboard.js')}}"></script>
+
+        @yield('scripts')
+        
+    </body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,53 @@
+<div class="page-sidebar sidebar">
+    <div class="page-sidebar-inner slimscroll">
+        <div class="sidebar-header">
+            <div class="sidebar-profile">
+                <a href="javascript:void(0);" id="profile-menu-link">
+                    <div class="sidebar-profile-image">
+                        <img src="{{ URL::asset('assets/images/profile-menu-image.png')}}" class="img-circle img-responsive" alt="">
+                    </div>
+                    <div class="sidebar-profile-details">
+                        <span>{{ Auth::user()->name }}<br><small>Art Director</small></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+            <ul class="menu accordion-menu">
+            <li id="dashboard" class="active"><a href="{{url('/home')}}" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-home"></span><p>Dashboard</p></a></li>
+            <li id="perfil"><a href="{{url('/perfil')}}" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-user"></span><p>Perfil</p></a></li>
+            <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-sitemap"></span><p>Empresa</p><span class="arrow"></span></a>
+                <ul class="sub-menu">
+                    <li><a href="{{url('/empresas/create')}}">Adicionar</a></li>
+                    <li><a href="{{url('/empresas')}}">Listar</a></li>
+                </ul>
+            </li>
+            <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-book"></span><p>Diarios</p><span class="arrow"></span></a>
+                <ul class="sub-menu">
+                    <li><a href="{{url('/diarios/create')}}">Adicionar</a></li>
+                    <li><a href="{{url('/diarios')}}">Listar</a></li>
+                </ul>
+            </li>
+            <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-calendar"></span><p>Plano de Contas</p><span class="arrow"></span></a>
+                <ul class="sub-menu">
+                    <li><a href="{{url('/planocontas/create')}}">Adicionar</a></li>
+                    <li><a href="{{url('/planocontas')}}">Listar</a></li>
+                </ul>
+            </li>
+            <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-file"></span><p>Docs. Pré-definidos</p><span class="arrow"></span></a>
+                <ul class="sub-menu">
+                    <li><a href="{{url('/docspredefinidos/create')}}">Adicionar</a></li>
+                    <li><a href="{{url('/docspredefinidos')}}">Listar</a></li>
+                </ul>
+            </li>
+            <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon fa fa-dollar"></span><p>Lançamentos</p><span class="arrow"></span></a>
+                <ul class="sub-menu">
+                    <li><a href="{{url('/lancamentos/create')}}">Adicionar</a></li>
+                    <li><a href="{{url('/lancamentos')}}">Listar</a></li>
+                </ul>
+            </li>
+        </ul>
+       
+        
+    </div><!-- Page Sidebar Inner -->
+</div><!-- Page Sidebar -->
+
