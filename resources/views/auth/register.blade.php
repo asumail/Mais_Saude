a3ad653ef32d2876450096a8fc4455a09969f2b0
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-10-01 14:01:18 +0200

    Alterando o register template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+<!--Css Local-->
+@section('cssLocal')
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+@endsection
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+<!--Conteudo Local-->
+@section('content') 
+        
+<div id="main-wrapper">
+                    <div class="row">
+                        <div class="col-md-3 center">
+                            <div class="login-box">
+                                <a href="index.html" class="logo-name text-lg text-center">Modern</a>
+                                <p class="text-center m-t-md">Create a Modern's account</p>
+                                <form class="m-t-md" method="POST" action="{{ route('register') }}">
+                                {{ csrf_field() }}
+                                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                        <input name="name" type="text" class="form-control" placeholder="Nome" required>
+                                        @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
+                                         @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <input name="email" type="email" class="form-control" placeholder="Email" required>
+                                        @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
+                                         @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <input name="password" type="password" class="form-control" placeholder="Senha" required>
+                                        @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                         @endif
+                                    </div>
+                                    <label>
+                                        <input type="checkbox"> Termos e condições aplicaveis.
+                                    </label>
+                                    <button type="submit" class="btn btn-success btn-block m-t-xs">Submit</button>
+                                    <p class="text-center m-t-xs text-sm">Ja possui uma conta?</p>
+                                    <a href="" class="btn btn-default btn-block m-t-xs">Login</a>
+                                </form>
+                                <p class="text-center m-t-xs text-sm">2018 &copy; Medical by MazasDaUEM.</p>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    </div><!-- Row -->
+                </div><!-- Main Wrapper -->
 @endsection
+<!--Fim do Conteudo Local-->
+
+<!--Javascript Local-->
+@section('scripts')
+
+@endsection
+<!-- fim Javascript Local-->
